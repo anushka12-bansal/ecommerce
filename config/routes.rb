dcba5684c1f1670959c23b0c1fcf9e1808970c3c
annu <anushka.bansal@poolstack.in>
2023-10-18 12:54:31 +0530

    create resources route for products

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   # post "users/", to: "users#create"
   resources :users
+  resources :products
   # resources :books
   # get "users/", to: "users#index"
   # get "users/new", to: "users#new"
