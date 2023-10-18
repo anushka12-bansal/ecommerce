291777b656408968b1af028d8eae1169dc8a86d7
annu <anushka.bansal@poolstack.in>
2023-10-18 12:44:45 +0530

    index, create apis

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+    def index
+     @product= Product.all 
+     render json: ProductSerializer.new(@product)
+    end
+
+    def create
+        byebug
+    @product= Product.new(product_params)
+    if @product.save
+        render json: ProductSerializer.new(@product)
+    else
+        render json: @product.errors
+    end
+end
+
+    private
+    def product_params
+        params.require(:product).permit(:name, :price, :manufacturing_date, :description,:expire_date,:manufactured_by)
+    end
 end
