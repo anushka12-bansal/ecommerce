250c904b85eb1319f69aa3c25ab42cd7536e1458
annu <anushka.bansal@poolstack.in>
2023-10-19 10:26:06 +0530

    add categories

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name , :price, :manufacturing_date,:description, :expire_date, :manufactured_by, :users
+  attributes :id, :name , :price, :manufacturing_date,:description, :expire_date, :manufactured_by, :users, :categories
 end
