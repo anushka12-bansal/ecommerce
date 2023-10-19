4809f5cca0f523bd839a559111fb6ed4b294221d
annu <anushka.bansal@poolstack.in>
2023-10-19 10:27:26 +0530

    user belongs to product

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id , :product, :first_name, :last_name, :email, :address, :city, :state, :adhar_number, :contact_number, :age 
+  attributes :id, :product, :first_name, :last_name, :email, :address, :city, :state, :adhar_number, :contact_number, :age 
 end
